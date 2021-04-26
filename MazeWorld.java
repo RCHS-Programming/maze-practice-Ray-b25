19daec642b7a601ff669cb8aef4c66b6fd8227e0
Ray-b25 <raymond.brandeberry@stu.boone.kyschools.us>
2021-04-26 08:11:15 -0400

    finished step 3

--- MazeWorld.java
+++ MazeWorld.java
@@ -17,5 +17,45 @@ public class MazeWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        for(int i = 0; i < 16; i++)
+        {
+            addObject( new Wall("edge.png"), 25 + 50*i, 12);
+        }
+        for(int i = 0; i < 16; i++)
+        {
+            addObject( new Wall("edge.png"), 25 + 50*i, 587);
+        }
+        for(int i = 0; i < 16; i++)
+        {
+            addObject( new Wall("edge2.png"), 12, 25 + 50*i);
+        }
+        for(int i = 0; i < 16; i++)
+        {
+            addObject( new Wall("edge2.png"), 787, 25 + 50*i);
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            addObject( new Wall("edge.png"), 25 + 50*i, 150);
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            addObject( new Wall("edge.png"), 25 + 50*i, 150);
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            addObject( new Wall("edge.png"), 325 + 50*i, 300);
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            addObject( new Wall("edge2.png"), 150, 325 + 50*i);
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            addObject( new Wall("edge.png"), 175 + 50*i, 425);
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            addObject( new Wall("edge2.png"), 575, 425 + 50*i);
+        }
     }
 }
